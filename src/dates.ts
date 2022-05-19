f82986d399dc700ed471eecf1219c6bd14925978
Irina <irina2358@yandex.ru>
2022-05-19 11:18:42 +0300

    исправлены недочеты

--- src/dates.ts
+++ src/dates.ts
@@ -1,13 +1,16 @@
 
+const ONE_DAY = 1
+const THREE_DAY = 3
+const ONE_MONTH = 1
+const TWO_MONTH = 2
 let today = new Date();
+let currDate = today.getDate().toString().padStart(2, "0");
+let currMonth = (today.getMonth() + ONE_MONTH).toString().padStart(2, "0");
+let nextMonth = (today.getMonth() + TWO_MONTH).toString().padStart(2, "0");
+let currYear = today.getFullYear();
+let lastDayDate = (new Date(today.getFullYear(), today.getMonth() + TWO_MONTH, 0)).toLocaleString('RU', { day: 'numeric' });
 
-let curr_date = today.getDate().toString().padStart(2, "0");
-let curr_month = (today.getMonth() + 1).toString().padStart(2, "0");
-let next_month = (today.getMonth() + 2).toString().padStart(2, "0");
-let curr_year = today.getFullYear();
-let lastDayDate = (new Date(today.getFullYear(), today.getMonth() + 2, 0)).toLocaleString('RU', { day: 'numeric' });
-
-export const todayDMY: string = `${curr_year}-${curr_month}-${curr_date}`
-export const defaultDateIn: string = `${curr_year}-${curr_month}-${today.getDate() + 1}`
-export const defaultDateOff: string = `${curr_year}-${curr_month}-${today.getDate() + 3}`
-export const lastDayNextMonth: string = `${curr_year}-${next_month}-${lastDayDate}`
+export const todayDMY: string = `${currYear}-${currMonth}-${currDate}`
+export const defaultDateIn: string = `${currYear}-${currMonth}-${today.getDate() + ONE_DAY}`
+export const defaultDateOff: string = `${currYear}-${currMonth}-${today.getDate() + THREE_DAY}`
+export const lastDayNextMonth: string = `${currYear}-${nextMonth}-${lastDayDate}`
